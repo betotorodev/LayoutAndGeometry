3a90703d0695ea39f26668663b6f571b8f43f020
Beto Toro <betotoro0902@gmail.com>
2022-10-31 15:35:44 -0500

    [feat] second challenge complete

--- LayoutAndGeometry/exercises/ScrollEfectView.swift
+++ LayoutAndGeometry/exercises/ScrollEfectView.swift
@@ -21,6 +21,7 @@ struct ScrollEfectView: View {
               .background(colors[index % 7])
               .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
               .opacity(geo.frame(in: .global).minY / 200)
+              .scaleEffect(max(0.5, geo.frame(in: .global).minY / 400))
               .onTapGesture {
                 print(geo.frame(in: .global).minY / 200)
               }
