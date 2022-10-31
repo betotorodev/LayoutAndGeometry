c1f11ca6b56ff2e7729a523fb6915b11d4b5df0e
Beto Toro <betotoro0902@gmail.com>
2022-10-31 15:39:44 -0500

    [feat] third challenge complete

--- LayoutAndGeometry/exercises/ScrollEfectView.swift
+++ LayoutAndGeometry/exercises/ScrollEfectView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ScrollEfectView: View {
-  let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+//  let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
   
   var body: some View {
     GeometryReader { fullView in
@@ -18,7 +18,7 @@ struct ScrollEfectView: View {
             Text("Row #\(index)")
               .font(.title)
               .frame(maxWidth: .infinity)
-              .background(colors[index % 7])
+              .background(Color(hue: min(1, geo.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
               .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
               .opacity(geo.frame(in: .global).minY / 200)
               .scaleEffect(max(0.5, geo.frame(in: .global).minY / 400))
